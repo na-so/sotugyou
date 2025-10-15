4f2166b36b8681870148ebe5022d0ab52a062f73
na-so <natori032@gmail.com>
2025-10-15 10:43:40 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,90 +1,71 @@
 /**
- * TextAlive App API script tag example
- * https://github.com/TextAliveJp/textalive-app-script-tag
+ * TextAlive App API basic example
+ * https://github.com/taisukef/textalive-app-basic
+ * forked from https://github.com/TextAliveJp/textalive-app-basic
  *
- * 発声中の歌詞をフレーズ単位で表示します。
+ * API チュートリアル「1. 開発の始め方」のサンプルコードを改変したものです。
+ * 発声中の歌詞を単語単位で表示します。
  * また、このアプリが TextAlive ホストと接続されていなければ再生コントロールを表示します。
- *
- * `script` タグで TextAlive App API を読み込んでいること以外は https://github.com/TextAliveJp/textalive-app-phrase と同内容です。
+ * https://developer.textalive.jp/app/
  */
 
-const { Player, Ease } = TextAliveApp;
+const { Player } = TextAliveApp;
 
-const player = new Player({
-  app: {
-    appAuthor: "Jun Kato",
-    appName: "Basic example"
-  },
-  mediaElement: document.querySelector("#media")
-});
+const songurl = "https://www.nicovideo.jp/watch/sm12825985";
 
-player.addListener({
-  onAppReady,
-  onTimerReady,
-  onTimeUpdate,
-  onThrottledTimeUpdate
-});
+const appAuthor = "Taisuke Fukuno";
+const appName = "Baisc Sample";
 
-const playBtn = document.querySelector("#play");
-const jumpBtn = document.querySelector("#jump");
-const pauseBtn = document.querySelector("#pause");
-const rewindBtn = document.querySelector("#rewind");
-const positionEl = document.querySelector("#position strong");
-
-const artistSpan = document.querySelector("#artist span");
-const songSpan = document.querySelector("#song span");
-const phraseEl = document.querySelector("#container p");
-const beatbarEl = document.querySelector("#beatbar");
-
-function onAppReady(app) {
-  if (!app.managed) {
-    document.querySelector("#control").style.display = "block";
-    playBtn.addEventListener("click", () => player.video && player.requestPlay());
-    jumpBtn.addEventListener("click", () => player.video && player.requestMediaSeek(player.video.firstPhrase.startTime));
-    pauseBtn.addEventListener("click", () => player.video && player.requestPause());
-    rewindBtn.addEventListener("click", () => player.video && player.requestMediaSeek(0));
-  }
-  if (!app.songUrl) {
-    player.createFromSongUrl("http://www.youtube.com/watch?v=ygY2qObZv24");
-  }
-}
-
-function onTimerReady() {
-  artistSpan.textContent = player.data.song.artist.name;
-  songSpan.textContent = player.data.song.name;
-
-  document
-    .querySelectorAll("button")
-    .forEach((btn) => (btn.disabled = false));
-
-  let p = player.video.firstPhrase;
-  jumpBtn.disabled = !p;
-
-  // set `animate` method
-  while (p && p.next) {
-    p.animate = animatePhrase;
-    p = p.next;
-  }
-}
-
-function onTimeUpdate(position) {
-
-  // show beatbar
-  const beat = player.findBeat(position);
-  if (!beat) {
-    return;
-  }
-  beatbarEl.style.width = `${Math.ceil(Ease.circIn(beat.progress(position)) * 100)}%`;
-}
-
-function onThrottledTimeUpdate(position) {
-  positionEl.textContent = String(Math.floor(position));
-}
-
-function animatePhrase(now, unit) {
-
-  // show current phrase
+const animateWord = (now, unit) => {
   if (unit.contains(now)) {
-    phraseEl.textContent = unit.text;
+    text.textContent = unit.text;
   }
 };
+
+const player = new Player({ app: { appAuthor, appName }, mediaElement: media });
+
+player.addListener({
+  onAppReady(app) {
+    if (!app.managed) {
+      control.style.display = "block";
+      const p = player;
+      document.querySelectorAll(".play").forEach((btn) => btn.onclick = () => p.video && p.requestPlay());
+      jump.onclick = () => p.video && p.requestMediaSeek(p.video.firstChar.startTime);
+      pause.onclick = () => p.video && p.requestPause();
+      rewind.onclick = () => p.video && p.requestMediaSeek(0);
+      document.querySelector("#header a").setAttribute("href", "https://developer.textalive.jp/app/run/?ta_app_url=https%3A%2F%2Ftextalivejp.github.io%2Ftextalive-app-basic%2F&ta_song_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DygY2qObZv24");
+    } else {
+      document.querySelector("#header a").setAttribute("href", "https://textalivejp.github.io/textalive-app-basic/");
+    }
+    if (!app.songUrl) {
+      player.createFromSongUrl(songurl);
+    }
+  },
+  onVideoReady(v) {
+    document.querySelector("#artist span").textContent = player.data.song.artist.name;
+    document.querySelector("#song span").textContent = player.data.song.name;
+    let w = player.video.firstWord;
+    while (w) {
+      w.animate = animateWord;
+      w = w.next;
+    }
+  },
+  onTimerReady(t) {
+    if (!player.app.managed) {
+      document.querySelectorAll("button").forEach((btn) => btn.disabled = false);
+    }
+    jump.disabled = !player.video.firstChar;
+  },
+  onThrottledTimeUpdate(pos) {
+    document.querySelector("#position strong").textContent = Math.floor(pos);
+  },
+  onPlay() {
+    overlay.style.display = "none";
+  },
+  onPause() {
+    text.textContent = "-";
+  },
+  onStop() {
+    text.textContent = "-";
+  },
+});
